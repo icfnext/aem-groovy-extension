6145ff298ba377d001fb87b874f1e0dc794080f3
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-02-28 10:36:21 -0600

    fix for test updates.

--- aem-groovy-extension-tests/src/test/groovy/com/citytechinc/aem/groovy/extension/AbstractGroovyExtensionSpec.groovy
+++ aem-groovy-extension-tests/src/test/groovy/com/citytechinc/aem/groovy/extension/AbstractGroovyExtensionSpec.groovy
@@ -1,28 +1,9 @@
 package com.citytechinc.aem.groovy.extension
 
 import com.citytechinc.aem.spock.specs.AbstractSlingRepositorySpec
-import com.citytechinc.aem.groovy.extension.builders.NodeBuilder
-import com.citytechinc.aem.groovy.extension.builders.PageBuilder
-import com.citytechinc.aem.groovy.extension.metaclass.GroovyExtensionMetaClassRegistry
 import com.day.cq.wcm.api.PageManager
-import spock.lang.Shared
 
 abstract class AbstractGroovyExtensionSpec extends AbstractSlingRepositorySpec {
-
-    @Shared nodeBuilder
-
-    @Shared pageBuilder
-
-    def setupSpec() {
-        GroovyExtensionMetaClassRegistry.registerMetaClasses()
-
-        nodeBuilder = new NodeBuilder(session)
-        pageBuilder = new PageBuilder(session)
-    }
-
-    def cleanupSpec() {
-        GroovyExtensionMetaClassRegistry.removeMetaClasses()
-    }
 
     def cleanup() {
         removeAllNodes()
