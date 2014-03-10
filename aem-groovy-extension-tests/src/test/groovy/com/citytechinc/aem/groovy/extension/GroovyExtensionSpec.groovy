1eef3577bebe28ebcbcc99022afc54c3aeb3d3c0
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-03-10 13:03:04 -0500

    test updates.

--- aem-groovy-extension-tests/src/test/groovy/com/citytechinc/aem/groovy/extension/GroovyExtensionSpec.groovy
+++ aem-groovy-extension-tests/src/test/groovy/com/citytechinc/aem/groovy/extension/GroovyExtensionSpec.groovy
@@ -1,6 +1,6 @@
 package com.citytechinc.aem.groovy.extension
 
-import com.citytechinc.aem.spock.specs.AemSpec
+import com.citytechinc.aem.prosper.specs.AemSpec
 import com.day.cq.wcm.api.PageManager
 
 abstract class GroovyExtensionSpec extends AemSpec {
