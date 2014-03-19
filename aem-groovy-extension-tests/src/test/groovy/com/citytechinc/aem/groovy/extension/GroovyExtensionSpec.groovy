e2df2806a02cab33f845aaef792830c55a9ffdbe
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-03-19 11:49:56 -0400

    test update.

--- aem-groovy-extension-tests/src/test/groovy/com/citytechinc/aem/groovy/extension/GroovyExtensionSpec.groovy
+++ aem-groovy-extension-tests/src/test/groovy/com/citytechinc/aem/groovy/extension/GroovyExtensionSpec.groovy
@@ -1,26 +1,10 @@
 package com.citytechinc.aem.groovy.extension
 
 import com.citytechinc.aem.prosper.specs.ProsperSpec
-import com.day.cq.wcm.api.PageManager
-import spock.lang.Shared
 
 abstract class GroovyExtensionSpec extends ProsperSpec {
 
-    @Shared featurePageManager
-
-    def setup() {
-        featurePageManager = resourceResolver.adaptTo(PageManager)
-    }
-
     def cleanup() {
         removeAllNodes()
-    }
-
-    def getNode(String path) {
-        session.getNode(path)
-    }
-
-    def getPage(String path) {
-        featurePageManager.getPage(path)
     }
 }
