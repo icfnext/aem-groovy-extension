4aa72234833a9260738e694bb7e530a5b3c452fa
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-10-03 09:42:16 -0500

    groovydoc updates.

--- aem-groovy-extension-bundle/src/main/groovy/com/citytechinc/aem/groovy/extension/services/ExtensionService.groovy
+++ aem-groovy-extension-bundle/src/main/groovy/com/citytechinc/aem/groovy/extension/services/ExtensionService.groovy
@@ -1,16 +1,17 @@
 package com.citytechinc.aem.groovy.extension.services
 
 /**
- * The extension service is responsible for aggregrating all metaclass extension providers and handling the dynamic
- * registration and removal of metaclass closures.
+ * The extension service is responsible for aggregrating all metaclass extension providers and handling the
+ * registration and removal of metaclass closures as these provider implementations are added or removed from the
+ * OSGi container.
  */
 interface ExtensionService {
 
     /**
      * Get the set of all classes that have associated metaclasses.  This value may change as metaclass extension
-     * providers are activated or disabled in the OSGi container.
+     * providers are added or removed from the OSGi container.
      *
-     * @return set of classes with registered metaclasses
+     * @return set of classes that have associated metaclasses registered in the container
      */
     Set<Class> getMetaClasses()
 }
